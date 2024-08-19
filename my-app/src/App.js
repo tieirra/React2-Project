d5d38d455104ae213691c574779305e4e8e1b507
Tieirra <167941085+Tieirra77@users.noreply.github.com>
2024-08-18 23:02:44 -0500

    adding click component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,18 @@
 //import './App.css';
 import React from 'react';
 import FunctionalComponent from "./Components/FunctionalComponent";
+import { ClassComp1 }from "./Components/ClassComp";
+import Click from "./Components/Click" ;
 
 function App() {
   return (
     <div>
    <h1> Hello Tieirra Single</h1>
-   <h1>Yes its true</h1>
+   <h1>Im the realest!</h1>
    <FunctionalComponent/>
+    {/* <ClassComp/> */}
+   <ClassComp1/>
+   <Click/>
    </div>
    
 
--- my-app/src/Components/ClassComp.js
+++ my-app/src/Components/ClassComp.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
-class Classcomp extends React.Component{
+export class ClassComp extends React.Component{
 
     render (){
         return <p> This is a  class compnent</p>
     }
 }
+
+export class ClassComp1 extends React.Component{
+
+    render (){
+        return <p> Hey im another class</p>
+    }
+}
+
