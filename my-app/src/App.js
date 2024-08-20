2a8dfa8895450285d858212c88939ab5fa35b451
Tieirra <167941085+Tieirra77@users.noreply.github.com>
2024-08-20 18:18:20 -0500

    adding counter component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,16 +4,18 @@ import React from 'react';
 import FunctionalComponent from "./Components/FunctionalComponent";
 import { ClassComp1 }from "./Components/ClassComp";
 import Click from "./Components/Click" ;
+import Counter from "./Components/Counter";
 
 function App() {
   return (
     <div>
-   <h1> Hello Tieirra Single</h1>
+   <h1> Hello Tieirra Marie</h1>
    <h1>Im the realest!</h1>
    <FunctionalComponent/>
     {/* <ClassComp/> */}
    <ClassComp1/>
    <Click/>
+   <Counter/>
    </div>
    
 
---
+++ my-app/src/Components/Counter.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+export class Counter extends Component {
+    constructor(props){
+        super(props);
+           this.state =  {
+
+            count: 0 
+        };
+    }
+
+    IncrementCount= () => {
+        this.setState  ({   count: this.state.count   + 1          })
+    }
+
+  render() {
+const {count} = this.state;
+
+    return (
+      <div>
+        <button onMouseEnter={ this.IncrementCount}> Incremented to {count} </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
